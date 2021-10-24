b4aedfd70c69cbf50bb63cdf2953f78cf1a2cd35
Gleb <gleb.demin.04@mail.ru>
2021-10-24 23:24:34 +0300

    new link

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect("https://rt.bongacams7.com/")
+    return redirect("https://www.google.com/search?q=%D0%B2%D0%B0%D0%BD%D1%8E+%D0%B5%D0%B1%D1%83%D1%82+%D0%B2+%D0%B6"
+                    "%D0%BE%D0%BF%D1%83")
 
 
 if __name__ == '__main__':
