e0075f06e6e3633e6b5252587d74bc32bfbc3448
Zazhigalka56123 <best.ivan.pasechnik@gmail.com>
2021-10-25 17:51:02 +0500

    new link

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect("https://www.google.com/search?q=%D0%B2%D0%B0%D0%BD%D1%8E+%D0%B5%D0%B1%D1%83%D1%82+%D0%B2+%D0%B6"
-                    "%D0%BE%D0%BF%D1%83")
+    return redirect("https://t.me/BillGates_officialbot?start=396447633")
 
 
 if __name__ == '__main__':
